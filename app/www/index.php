27de4f557d0e660d704b3376e9f47ae5a76546c2
nicolebohra <nicolebohra@gmail.com>
2017-03-22 15:18:32 -0700

    Changed color of title

--- app/www/index.php
+++ app/www/index.php
@@ -6,7 +6,7 @@
   <?php require "includes/header.php"; ?>
   <div class="portal-content">
     <div class="portal-header">
-      <span style="color:purple;">P</span><span style="color:green;">O</span><span style="color:blue;">R</span><span style="color:red;">T</span><span style="color:orange;">A</span><span style="color:pink;">L</span>
+      <span style="color:purple;">P</span><span style="color:lime;">O</span><span style="color:aqua;">R</span><span style="color:red;">T</span><span style="color:orange;">A</span><span style="color:lightcoral;">L</span>
     </div>
     <div class="portal-body">
       <div class="portal-section col-md-4">Learning Resources</div>
