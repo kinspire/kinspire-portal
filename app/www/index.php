2ba5a381ba6570bdb680ea6fc329c77969030034
nicolebohra <nicolebohra@gmail.com>
2017-03-22 13:47:24 -0700

    changed column name

--- app/www/index.php
+++ app/www/index.php
@@ -9,9 +9,9 @@
       <span style="color:purple;">P</span><span style="color:green;">O</span><span style="color:blue;">R</span><span style="color:red;">T</span><span style="color:orange;">A</span><span style="color:pink;">L</span>
     </div>
     <div class="portal-body">
-      <div class="portal-section col-md-4">Column 1</div>
-      <div class="portal-section col-md-4">Column 2</div>
-      <div class="portal-section col-md-4">Column 3</div>
+      <div class="portal-section col-md-4">Learning Resources</div>
+      <div class="portal-section col-md-4">Activites</div>
+      <div class="portal-section col-md-4">Volunteer Access</div>
     </div>
   </div>
 </body>
