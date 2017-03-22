f422769b8c9b44e18763451b04017b9e77396bcd
Sarang Joshi <sarangj@msn.com>
2017-03-21 20:53:32 -0700

    Column template

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # kinspire-portal
 A desktop portal for the students in Kinspire's orphanages to use to access learning materials.
+
+## Installation
+- Clone the repository.
+- If on a windows platform, run `./win-setup.sh` on Git BASH.
+- The source directory is `app/www/`.
--- app/www/index.php
+++ app/www/index.php
@@ -1,9 +1,16 @@
 <head>
-  <link rel="stylesheet" href="styles/style.css" type="text/css"/>
+  <?php require "includes/style.php"; ?>
 </head>
 <body>
   <?php require "includes/header.php"; ?>
-  <div class="portal-header">
-    <span style="color:purple;">P</span><span style="color:green;">O</span><span style="color:blue;">R</span><span style="color:red;">T</span><span style="color:orange;">A</span><span style="color:pink;">L</span>
+  <div class="portal-content">
+    <div class="portal-header">
+      <span style="color:purple;">P</span><span style="color:green;">O</span><span style="color:blue;">R</span><span style="color:red;">T</span><span style="color:orange;">A</span><span style="color:pink;">L</span>
+    </div>
+    <div class="portal-body">
+      <div class="portal-section col-md-4">Column 1</div>
+      <div class="portal-section col-md-4">Column 2</div>
+      <div class="portal-section col-md-4">Column 3</div>
+    </div>
   </div>
 </body>
