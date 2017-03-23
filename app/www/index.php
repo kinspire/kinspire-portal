e588b67b615db76170f3f24cbc7b6a417c5b8413
Sarang Joshi <sarangj@msn.com>
2017-03-23 03:54:22 -0700

    Test

--- app/www/index.php
+++ app/www/index.php
@@ -27,6 +27,16 @@
       </div>
     </div>
     <?php require $_SERVER['DOCUMENT_ROOT']."/includes/menu.php";?>
+    <div class="portal-menu">
+      <div class="portal-menu-title" id="menu-title">Menu</div>
+      <div class="portal-menu-content" id="menu-content">
+        <div><a href="/resources/">Learning Resources</a></div>
+        <div>Activities</div>
+        <div>Volunteer Access</div>
+        <div>Profile</div>
+        <div>Contact (Nicole is awesome!!!)</div>
+      </div>
+    </div>
   </div>
   <?php require $_SERVER['DOCUMENT_ROOT']."/includes/footer.php"; ?>
 </body>
