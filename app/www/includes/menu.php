f218b5153923260f67ba38e1aa674c57c1960a53
nicolebohra <nicolebohra@gmail.com>
2017-03-26 12:37:23 -0700

    changed up menu

--- app/www/includes/menu.php
+++ app/www/includes/menu.php
@@ -1,10 +1,14 @@
 <div class="portal-menu">
-  <div class="portal-menu-title" id="menu-title">Menu</div>
+  <div class="portal-menu-title" id="menu-title">
+    <!-- <img src="/images/owl.png"/> -->
+    Menu
+  </div>
   <div class="portal-menu-content" id="menu-content">
-    <div><a href="/resources/">Learning Resources</a></div>
-    <div>Activities</div>
-    <div>Volunteer Access</div>
-    <div>Profile</div>
-    <div>Contact</div>
+    <div class="portal-menu-item"><a href="/resources/">Learning Resources</a></div>
+    <div class="portal-menu-item">Activities</div>
+    <div class="portal-menu-item">Volunteer Access</div>
+    <div class="portal-menu-item">Profile</div>
+    <div class="portal-menu-item">Contact</div>
+    <div class="portal-menu-item">Kinspire UW- Contact us: kinspire.uw.edu 2017</div>
   </div>
 </div>
--- app/www/index.php
+++ app/www/index.php
@@ -1,16 +1,16 @@
 <head>
   <?php require $_SERVER['DOCUMENT_ROOT']."/includes/style.php"; ?>
-  <title>Portal</title>
+  <title>Welcome</title>
 </head>
 <body>
   <?php require $_SERVER['DOCUMENT_ROOT']."/includes/header.php"; ?>
   <div class="portal-content">
-    <div class="portal-header">Portal</div>
+    <div class="portal-header">Welcome</div>
     <div class="portal-body container">
       <div class="home-section col-xs-4">
         <div class="home-section-date">
           <div class="home-section-title">Today's date</div>
-          <div class="home-section-content"><?php $date = date('d/m/Y');echo $date; ?></div>
+          <div class="home-section-content">03/24/17</div>
         </div>
         <div class="home-section-quote">
           <div class="home-section-title">Quote of the Day</div>
@@ -27,16 +27,6 @@
       </div>
     </div>
     <?php require $_SERVER['DOCUMENT_ROOT']."/includes/menu.php";?>
-    <div class="portal-menu">
-      <div class="portal-menu-title" id="menu-title">Menu</div>
-      <div class="portal-menu-content" id="menu-content">
-        <div><a href="/resources/">Learning Resources</a></div>
-        <div>Activities</div>
-        <div>Volunteer Access</div>
-        <div>Profile</div>
-        <div>Contact (Nicole is awesome!!!)</div>
-      </div>
-    </div>
   </div>
   <?php require $_SERVER['DOCUMENT_ROOT']."/includes/footer.php"; ?>
 </body>
