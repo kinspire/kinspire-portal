825a8e06fad5bea7bce0bf716acb3ac90cb96eb4
Sarang Joshi <sarangj@msn.com>
2017-05-12 03:04:00 -0700

    Python to PHP, due to UTF problems, wahoo

---
+++ app/www/content/story.php
@@ -0,0 +1,14 @@
+<?php
+$STORY_NUMBER = 1;
+$CONTENT_FOLDER = 'stories/'; 
+
+$filename = $CONTENT_FOLDER.$STORY_NUMBER.'.json';
+$outputFile = fopen($CONTENT_FOLDER.$STORY_NUMBER.'.html', 'w');
+$story_json = json_decode(file_get_contents($filename), true);
+
+$paragraphs = $story_json['story'];
+foreach ($paragraphs as $paragraph) {
+    fwrite($outputFile, $paragraph);
+    fwrite('<br/><br/>');
+}
+?>
