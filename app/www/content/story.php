f169c3fe0c5886da4a61a8e523064c3064bd9749
Sarang Joshi <sarangj@msn.com>
2017-05-14 21:53:08 -0700

    Split into words

--- app/www/content/story.php
+++ app/www/content/story.php
@@ -1,4 +1,6 @@
 <?php
+echo "helllo";
+
 $STORY_NUMBER = 1;
 $CONTENT_FOLDER = 'stories/';
 
@@ -9,6 +11,11 @@ $story_json = json_decode(file_get_contents($filename), true);
 $paragraphs = $story_json['story'];
 
 foreach ($paragraphs as $paragraph) {
+  // Split into words -- i.e. any non-alphanumeric characters and "-"
+  var_dump($paragraph);
+  $words = preg_split("\\w+", $paragraph);
+  var_dump($words);
+  break;
   fwrite($outputFile, $paragraph);
   fwrite($outputFile, '<br/><br/>');
 }
--- app/www/content/story.py
+++ app/www/content/story.py
@@ -1,3 +1,4 @@
+import re
 import json
 
 STORY_NUMBER = '1'
@@ -9,7 +10,10 @@ story_json = json.load(filename)
 outputFile = open(CONTENT_FOLDER + STORY_NUMBER + '.html', 'w');
 
 paragraphs = story_json['story'];
+vocab = story_json['vocab'];
 
 for paragraph in paragraphs:
+  words = re.split("\W+", paragraph)
+  print(words)
   outputFile.write(paragraph);
   outputFile.write('<br/>BREAK<br/>');
