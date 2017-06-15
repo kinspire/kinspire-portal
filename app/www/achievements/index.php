fc6a921c22a9a47ea9bc2bd484ad087e544990ec
rajasagashe <rajas@cs.washington.edu>
2017-06-15 12:10:14 -0700

    added medals and colored fonts to achievements page

--- app/www/achievements/index.php
+++ app/www/achievements/index.php
@@ -1,23 +1,38 @@
 <?php require $_SERVER['DOCUMENT_ROOT']."/includes/scaffolder.php";
 head('Achievements', 4);?>
 <div class="portal-body">    
-    <div class="filler">Coming soon!</div>
-    <!--<div class="row justify-content-start">
-        <div class="col-xs-2">
-            Creating Account
+    <!-- <div class="filler">Coming soon!</div> -->
+    <div class="row justify-content-start">
+        <div class="home-section col-xs-2 col-md-offset-1">
+            <div class="medal">
+                <img class="achievements-section-medal-image" src="/images/home-page-medal-magnet.png"/>
+                <div class="home-section-title">Creating Account</div>
+            </div>
         </div>
         <div class="col-xs-2">
-            First assignment
+            <div class="medal">
+                <img class="achievements-section-medal-image" src="/images/home-page-medal-magnet.png"/>
+                <div class="home-section-title red-font">First assignment</div>
+            </div>
         </div>
         <div class="col-xs-2">
-            Choosing an avatar
+            <div class="medal">
+                <img class="achievements-section-medal-image" src="/images/home-page-medal-magnet.png"/>
+                <div class="home-section-title pink-font">Choosing an avatar</div>
+            </div>
         </div>
         <div class="col-xs-2">
-            Adding clothing
+            <div class="medal">
+                <img class="achievements-section-medal-image" src="/images/home-page-medal-magnet.png"/>
+                <div class="home-section-title yellow-font">Adding clothing</div>
+            </div>
         </div>
         <div class="col-xs-2">
-            Passing first quiz
+            <div class="medal">
+                <img class="achievements-section-medal-image" src="/images/home-page-medal-magnet.png"/>
+                <div class="home-section-title blue-font">Passing first quiz</div>
+            </div>
         </div>
-    </div>-->
+    </div>
 </div>
 <?php tail();?>
