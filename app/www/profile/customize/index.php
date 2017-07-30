5f7440b4fff301d881b9a758c2590cd4edb207a9
nicolebohra <nicolebohra@gmail.com>
2017-07-30 11:22:55 -0700

    custom page

--- app/www/profile/customize/index.php
+++ app/www/profile/customize/index.php
@@ -1,11 +1,16 @@
 <?php require $_SERVER['DOCUMENT_ROOT']."/includes/scaffolder.php";
 head('Your Avatar', 4); //
 ?>
-<div class= "portal-body">
-<span class="boxset">
-  <span class="box">Color</span>
-  <span class="box">Shoes</span>
-  <span class="box">Accessories</span>
-</span>
+<div class="portal-body flexbox">
+  <div class="profile-avatar">
+    <img class="profile-avatar-img" src="/images/avatar/<?php echo $user['avatar'];?>.svg"/>
+  </div>
+  <span class="portal-body flexbox">
+    <span class="boxset">
+      <span class="box">Color</span>
+      <span class="box">Shoes</span>
+      <span class="box">Accessories</span>
+    </span>
+  </span>
 </div>
 <?php tail(); ?>
