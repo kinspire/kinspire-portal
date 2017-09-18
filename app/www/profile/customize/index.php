f799eb9ff87932f48b9a73c615985ca678191a4e
nicolebohra <nicolebohra@gmail.com>
2017-09-18 14:44:01 -0700

    edits to avatar

--- app/www/profile/customize/index.php
+++ app/www/profile/customize/index.php
@@ -6,7 +6,7 @@ $user = $_SESSION['user'];
   <div class="profile-avatar">
     <img class="profile-avatar-img" src="/images/avatar/<?php echo $user['avatar'];?>.svg"/>
   </div>
-  <span class="portal-body flexbox">
+  <div class=flexbox>
     <span class="boxset">
       <div class = "dropdown">
         <span class="box1">Color</span>
@@ -18,24 +18,25 @@ $user = $_SESSION['user'];
         </div>
       </div>
       <div class ="dropdown">
-      <span class="box2">Shoes</span>
-      <div class="dropdown-content">
-        <img class = "shoes-1"src = "/images/Grey Sneakers.png">
-        <img class = "shoes-2"src = "/images/Blue Sneakers.png">
-        <img class = "shoes-3"src = "/images/Green Sneakers.png">
-        <img class = "shoes-4"src = "/images/Orange Sneakers.png">
+        <span class="box2">Shoes</span>
+        <div class="dropdown-content">
+          <img class = "shoes-1"src = "/images/Grey Sneakers.png">
+          <img class = "shoes-2"src = "/images/Blue Sneakers.png">
+          <img class = "shoes-3"src = "/images/Green Sneakers.png">
+          <img class = "shoes-4"src = "/images/Orange Sneakers.png">
+        </div>
       </div>
-    </div>
-    <div class="dropdown">
-      <span class="box3">Accessories</span>
-      <div class ="dropdown-content">
-        <img class = "shirt-1"src = "/images/Blue Shirt.png">
-        <img class = "shirt-2"src = "/images/Green Tank.png">
-        <img class = "shirt-3"src = "/images/Red Shirt.png">
-        <img class = "shirt-4"src = "/images/Grey tank.png">
+      <div class="dropdown">
+        <span class="box3">Accessories</span>
+        <div class ="dropdown-content">
+          <img class = "shirt-1"src = "/images/Blue Shirt.png">
+          <img class = "shirt-2"src = "/images/Green Tank.png">
+          <img class = "shirt-3"src = "/images/Red Shirt.png">
+          <img class = "shirt-4"src = "/images/Grey tank.png">
+        </div>
       </div>
-    </div>
     </span>
-  </span>
+  </div>
+ </div>
 </div>
 <?php tail(); ?>
--- app/www/profile/index.php
+++ app/www/profile/index.php
@@ -10,14 +10,15 @@ $birthday_str = $user['birthday']; // "Birthday str";
 $number = $user['class_level'];
 $ends = array('th','st','nd','rd','th','th','th','th','th','th');
 if (($number %100) >= 11 && ($number%100) <= 13)
-   $abbreviation = $number. 'th';
+$abbreviation = $number. 'th';
 else
-   $abbreviation = $number. $ends[$number % 10];
+$abbreviation = $number. $ends[$number % 10];
 $class_ord = $abbreviation;
 ?>
 <div class="portal-body flexbox">
   <div class="profile-avatar">
     <img class="profile-avatar-img" src="/images/avatar/<?php echo $user['avatar'];?>.svg"/>
+    <a href="/profile/customize/"><div class="customize">CUSTOMIZE ME!</div></a>
   </div>
   <div class="profile-details">
     <div>Hello,
@@ -28,8 +29,5 @@ $class_ord = $abbreviation;
     <div>You are in <span class="profile-detail"><?php echo $class_ord;?></span> class.<br></br></div>
     <div class= "profile-progress">
       <div>Check out your progress!</div>
-  </div>
-  <span class="customize-box">
-  <span class="customize">Customize Me!</span>
-</span>
-<?php tail(); ?>
+    </div>
+    <?php tail(); ?>
