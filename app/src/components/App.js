c14480653bc056357adfa6f1cfce57247608768d
Sarang Joshi <sarangj@msn.com>
2018-05-06 02:58:55 -0700

    Rough around the edges home page

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,14 +1,30 @@
+// @flow
 import React, { Component } from 'react';
 import './App.css';
+import top from '../images/portal-top-bar.png';
+import left from '../images/portal-left-bar.png';
+import bottom from '../images/portal-bottom-bar.png';
+import right from '../images/portal-right-bar.png';
 
 class App extends Component {
   props: {
     children: Children
   };
 
+  // TODO set content height on resize
+
   render() {
+    // TODO: menu and back button
+    // TODO: title component with the explode logic from PHP
     return (
-      <div>
+      <div id="portal-content" style={{"min-height": window.innerHeight, "max-height": window.innerHeight}}>
+        <img id="portal-background-top" src={top}/>
+        <img id="portal-background-bottom" src={bottom}/>
+        <img id="portal-background-left" src={left}/>
+        <img id="portal-background-right" src={right}/>
+        <div id="portal-header">
+          <div className="portal-title">{ this.props.title }</div>
+        </div>
         {this.props.children}
       </div>
     );
--- app/src/routes.js
+++ app/src/routes.js
@@ -2,15 +2,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './components/App';
-import Background from './components/Background';
 import Home from './components/Home';
 
 export default () => (
-  <App>
-    <Background title="Kinspire Portal">
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
-    </Background>
+  <App title="Kinspire Portal">
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
   </App>
 );
