6ec15e04a669386a9bf28c3be96372d11ec55d5e
mteshmore <moree@uw.edu>
2019-02-06 18:30:07 -0800

    updating recent changes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,9 +3,10 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import "./Menu.css";
+import {Navbar, Nav} from 'react-bootstrap'
 import menuMagnetInactive from "../images/owl-magnet-inactive.png";
 
-// The menu component that is rendered in the upper left corner at all times.
+// The menu component that is rendered in the upper portion at all times.
 class Menu extends Component {
   constructor(props) {
     super(props);
@@ -25,7 +26,7 @@ class Menu extends Component {
   render() {
     const sections = ["home", "resources", "activities", "access", "profile", "logout"];
     const urls = ["", "/materials", "/activities", "/volunteer-access", "/profile", "/login"];
-    const names = ["Home", "Materials", "Activities", "Volunteer Access", "Profile", "Log out"];
+    const names = ["PORTAL", "materials", "activities", "volunteer access", "profile", "logout"];
 
     const menuItems = sections.map((section, i) => (
       <div key={i} className="portal-menu-item">
@@ -36,6 +37,7 @@ class Menu extends Component {
       </div>
     ));
 
+    //** Make this the footer **
     const menuContent = (
       <div className="portal-menu-content" id="menu-content">
         {menuItems}
@@ -46,15 +48,19 @@ class Menu extends Component {
     );
 
     return (
-      <div className="portal-menu">
-        <div className="portal-menu-title" onClick={this.handleClick} id="menu-title">
-          <div>menu</div>
-          <div>
-            <img alt="" id="menu-magnet" src={menuMagnetInactive} height="60" width="60"/>
-          </div>
-        </div>
-        {this.state.open ? menuContent : ""}
-      </div>
+
+    <div class="menu">
+      {menuItems}
+    </div>
+      // <div className="portal-menu">
+      //   <div className="portal-menu-title" onClick={this.handleClick} id="menu-title">
+      //     <div>menu</div>
+      //     <div>
+      //       <img alt="" id="menu-magnet" src={menuMagnetInactive} height="60" width="60"/>
+      //     </div>
+      //   </div>
+      //   {this.state.open ? menuContent : ""}
+      // </div>
     );
   }
 }
