559f49f31ed4fa4733369583ae1021fc1c6df6bd
mteshmore <moree@uw.edu>
2019-05-02 21:56:02 -0700

    merged with anuraag

--- src/pages/Selection.jsx
+++ src/pages/Selection.jsx
@@ -31,7 +31,6 @@ class Selection extends Component {
   }
 
   // Handle changes in the view prop - we need to reload the items
-<<<<<<< HEAD
   // componentDidUpdate(prevProps) {
   //   if (prevProps.view !== this.props.view) {
   //     this.componentDidMount();
@@ -62,27 +61,6 @@ class Selection extends Component {
     } else if (this.props.view === viewConstants.ABOUT) {
       mainStyle.backgroundColor = "#a586c5";
     }
-=======
-  componentDidUpdate(prevProps) {
-    if (prevProps.view !== this.props.view) {
-      this.componentDidMount();
-    }
-    console.log(this.props.view);
-    if(this.props.view === viewConstants.MATERIALS) {
-      document.body.style.backgroundColor = '#a9bb59';
-    } else if(this.props.view === viewConstants.ACTIVITIES) {
-      document.body.style.backgroundColor = '#79b4b3';
-    } else if(this.props.view === viewConstants.WORDSEARCH) {
-      document.body.style.backgroundColor = '#79b4b3';
-    } else if(this.props.view === viewConstants.HELP) {
-      document.body.style.backgroundColor = '#fc5e5a';
-    } else if(this.props.view === viewConstants.ABOUT) {
-      document.body.style.backgroundColor = '#a586c5';
-    } 
-  }
-
-  render() {
->>>>>>> ui
     const itemsRendered = this.state.items.map(item => (
       <Link key={item.link} className="selection-category" to={item.link}>
         <div className="selection-category-content">
