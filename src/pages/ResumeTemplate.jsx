06f50490a475bd6191c7d0c18f57dc782f8120d9
Nicole Bohra <nicolebohra@Nicoles-MacBook-Pro-2.local>
2019-06-11 22:34:59 -0700

    resume details

--- src/pages/ResumeTemplate.jsx
+++ src/pages/ResumeTemplate.jsx
@@ -16,7 +16,7 @@ class ResumeTemplate extends Component {
     render(){
         return(
             <div>
-                <div style={{ textAlign: 'center', marginBottom: 10 }}><button onClick={this.exportPDF} style={{ margin: 'auto' }}>download</button></div>
+                {/* <div style={{ textAlign: 'center', marginBottom: 10 }}><button onClick={this.exportPDF} style={{ margin: 'auto' }}>download</button></div>
                 <PDFExport paperSize={'Letter'}
                     fileName="_____.pdf"
                     title=""
@@ -37,7 +37,34 @@ class ResumeTemplate extends Component {
                             </div>
 
                             
-                </PDFExport>                       
+                </PDFExport>                        */}
+                <h1>What is a resume?</h1>
+                <h4>A resume is a document that outlines your personal, educational, and 
+                    professional acheivements and qualifications when you apply for a job! </h4>
+                <h1>What is the purpose of it?</h1>
+                <h4>Resumes are a summary of your experiences which is one of the most
+                    important things employers look at when you apply to jobs. If the hiring manager likes 
+                    your resume and thinks your experiences will match the job qualifications, then you have a high chance of getting hired.
+                </h4>
+                <h1>What goes in a resume?</h1>
+                <h4>What goes in a resume is different based on what you're studying and what type of job you are applying
+                    to. Here are some general components of a resume:
+                </h4>
+                <li>Education Details</li>
+                <li>Any Work Experience (part-time jobs, full-time, job shadowing, etc.)</li>
+                <li>Any Project Experiences</li>
+                <li>Any Volunteer/Leadership Experiences</li>
+                <li>Technical Skills (skills that you will use on the job)</li>
+                <li>Personal Skills (skills that make you a great addition to the company/team)</li>
+                
+                <h1>Helpful Tips:</h1>
+                    <li>Keep your resume one page long</li>
+                    <li>Use headers and subheaders to make content clear</li>
+                    <li>Any Leadership Experiences</li>
+
+                <h1>Here is an example of a resume:</h1>
+                
+                
             </div>
            
         )
