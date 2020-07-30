862ea627fc8cb2425ac718ece2b96a97a17f5d47
Nicole Bohra <bohran@uw.edu>
2020-07-30 15:24:21 -0700

    added more health modules

--- src/client/Sample.ts
+++ src/client/Sample.ts
@@ -31,27 +31,55 @@ const english: Course = {
 
 // sample health course
 const lesson1: Lesson = {
-    title: "Nutrition",
+    title: "Body Hygiene Introduction",
     id: "lesson1-1",
     content: "blah",
 };
-const fuelingYourBody: Module = {
-    title: "Module 1: Nutrition",
-    id: "level1",
-    lessons: [lesson1],
+const lesson2: Lesson = {
+    title: "Healthy Hair",
+    id: "lesson1-2",
+    content: "blah",
 };
-const healthTier: Tier = {
+const lesson3: Lesson = {
+    title: "Excellent Eyes",
+    id: "lesson1-3",
+    content: "blah",
+};
+const lesson4: Lesson = {
+    title: "Exceptional Ears",
+    id: "lesson1-4",
+    content: "blah",
+};
+const lesson5: Lesson = {
+    title: "Nice Nose",
+    id: "lesson1-5",
+    content: "blah",
+};
+const module1: Module = {
+    title: "Module 1: Body Hygiene",
+    id: "level1",
+    lessons: [lesson1, lesson2, lesson3, lesson4, lesson5],
+};
+const module2: Module = {
+    title: "Module 2: Body Conditions",
+    id: "level2",
+    lessons: [],
+};
+const preventingIllness: Tier = {
+    title: "Preventing Illness",
+    id: "level1",
+    modules: [module1, module2],
+};
+const fuelingYourBody: Tier = {
     title: "Fueling your Body", 
     id: "tiers", 
-    modules: [fuelingYourBody],
+    modules: [],
 }
 const health: Course = {
     title: "Health",
     id: "health",
-    tiers: [healthTier],
+    tiers: [preventingIllness, fuelingYourBody],
 }
-
-
 
 export const courses = [
     english, health
