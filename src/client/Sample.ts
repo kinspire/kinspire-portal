b500f4becba51d109ae695338b09cd4a4c764fc6
Nicole Bohra <bohran@uw.edu>
2020-07-31 21:21:17 -0700

    added complete module

--- src/client/Sample.ts
+++ src/client/Sample.ts
@@ -30,35 +30,66 @@ const english: Course = {
 }
 
 // sample health course
-const lesson1: Lesson = {
+const hygeine1: Lesson = {
     title: "Body Hygiene Introduction",
     id: "lesson1-1",
     content: "blah",
 };
-const lesson2: Lesson = {
+const hygiene2: Lesson = {
     title: "Healthy Hair",
     id: "lesson1-2",
     content: "blah",
 };
-const lesson3: Lesson = {
+const hygeine3: Lesson = {
     title: "Excellent Eyes",
     id: "lesson1-3",
     content: "blah",
 };
-const lesson4: Lesson = {
+const hygeine4: Lesson = {
     title: "Exceptional Ears",
     id: "lesson1-4",
     content: "blah",
 };
-const lesson5: Lesson = {
+const hygeine5: Lesson = {
     title: "Nice Nose",
     id: "lesson1-5",
     content: "blah",
 };
+const hygeine6: Lesson = {
+    title: "Magical Mouth",
+    id: "lesson1-6",
+    content: "blah",
+};
+const hygeine7: Lesson = {
+    title: "Happy Hands",
+    id: "lesson1-7",
+    content: "blah",
+};
+const hygeine8: Lesson = {
+    title: "Neat Nails",
+    id: "lesson1-8",
+    content: "blah",
+};
+const hygeine9: Lesson = {
+    title: "Fabulous Feet",
+    id: "lesson1-9",
+    content: "blah",
+};
+const hygeine10: Lesson = {
+    title: "Cleaning your body!",
+    id: "lesson1-10",
+    content: "blah",
+};
+const hygeine11: Lesson = {
+    title: "Recap",
+    id: "lesson1-11",
+    content: "blah",
+};
+
 const module1: Module = {
     title: "Module 1: Body Hygiene",
     id: "level1",
-    lessons: [lesson1, lesson2, lesson3, lesson4, lesson5],
+    lessons: [hygeine1, hygiene2, hygeine3, hygeine4, hygeine5, hygeine6, hygeine7, hygeine8, hygeine9, hygeine9, hygeine10, hygeine11],
 };
 const module2: Module = {
     title: "Module 2: Body Conditions",
